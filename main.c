e8e0c4fa01d933e6808a889b4e7a5339c653eb53
Ethan J. Johnson <ethanjohnson89@gmail.com>
2014-03-21 12:40:14 -0400

    Reworked RR implementation to fix bug
    Initially this was going to be a small change, to fix some minor errors
    in scheduling order and timing - but, it ended up requiring a full
    rewrite (not modeled after any of the other mode functions), due to some
    subtleties in the RR scheme. The best way ended up being to simulate the
    actual ready queue used by a live scheduler.

--- main.c
+++ main.c
@@ -19,6 +19,13 @@ struct Process_sort {
 	struct Process_sort *next;
 };
 
+// For round-robin scheduling
+struct ReadyQueueNode {
+	struct Process *proc;
+	struct ReadyQueueNode *next;
+	int timeRemaining;
+};
+
 struct cpuSlot {
 	int procId;
 	int startTime;
@@ -197,118 +204,7 @@ struct cpuSlot* fcfs(struct Process* root, int quantum, int* downTime) {
 	}
 	return summaryList;
 }
-struct cpuSlot* rr(struct Process* root, int quantum, int* downTime) {
-	
-	//set up pointer to traverse list
-	struct Process* current;
-	current = root;
-	
-	struct cpuSlot* rootSlot;
-	struct cpuSlot* currentSlot;
-	rootSlot = malloc(sizeof(struct cpuSlot));
-	rootSlot->next = 0;
-	currentSlot = rootSlot;
-	
-	int prevDuration = 0;
-	int prevStart = 0;
-	
-	// set the root
-	currentSlot->procId = current->id;
-	if ((prevStart + prevDuration) < current->arrive) {
-		currentSlot->startTime = current->arrive;
-	} else {
-		currentSlot->startTime = prevStart + prevDuration;
-	}
-	if (current->duration < quantum) {
-		currentSlot->duration = current->duration;
-	} else {
-		currentSlot->duration = quantum;
-	}
-	currentSlot->wait = currentSlot->startTime - current->arrive;
-	currentSlot->end = currentSlot->startTime + currentSlot->duration;
-	currentSlot->turnaround = (currentSlot->end) - current->arrive;
-	if (current->arrive > (prevStart + prevDuration)) {
-		*downTime += current->arrive - (prevStart + prevDuration);
-	}
-	prevStart = currentSlot->startTime;
-	prevDuration = currentSlot->duration;
-	currentSlot->next = 0;
-	
-	//now add each node to the list
-	if (current->next == 0 && current->duration > quantum) { // this if else is to account for the case that there is only one process and its duration is larger than quantum
-		
-		// only one process exists in the list, and its duration is greater than quantum
-		current->duration -= quantum;
-		while (current->duration > 0) {
-			currentSlot->next = malloc(sizeof(struct cpuSlot));
-			currentSlot = currentSlot->next;
-			currentSlot->procId = current->id;
-			currentSlot->startTime = prevStart + prevDuration;
-			if (current->duration < quantum) {
-				currentSlot->duration = current->duration;
-			} else {
-				currentSlot->duration = quantum;
-			}
-			currentSlot->wait = current->arrive;
-			currentSlot->end = currentSlot->startTime + currentSlot->duration;
-			currentSlot->turnaround = (currentSlot->end) - (current->arrive + prevStart + prevDuration);
-			prevStart = currentSlot->startTime;
-			prevDuration = currentSlot->duration;
-			currentSlot->next = 0;
-			current->duration -= quantum;
-		}
-	} else {
-		while (current->next != 0) {
-			currentSlot->next = malloc(sizeof(struct cpuSlot));
-			currentSlot = currentSlot->next;
-			current = current->next;
-
-			currentSlot->procId = current->id;
-			if ((prevStart + prevDuration) < current->arrive) {
-				currentSlot->startTime = current->arrive;
-			} else {
-				currentSlot->startTime = prevStart + prevDuration;
-			}
-			if (current->duration < quantum) {
-				currentSlot->duration = current->duration;
-			} else {
-				currentSlot->duration = quantum;
-			}
-			currentSlot->wait = currentSlot->startTime - current->arrive;
-			currentSlot->end = currentSlot->startTime + currentSlot->duration;
-			currentSlot->turnaround = (currentSlot->end) - current->arrive;
-			if (current->arrive > (prevStart + prevDuration)) {
-				*downTime += current->arrive - (prevStart + prevDuration);
-			}
-			prevStart = currentSlot->startTime;
-			prevDuration = currentSlot->duration;
-			currentSlot->next = 0;
-			
-			if (current->duration > quantum) {
-				//we need a new node
-				//save what node we are on so we can get at its values
-				struct Process* location = current;
-				//traverse to end of process list
-				while (current->next != 0) {
-					current = current->next;
-				}
-				//add new node to the list
-				current->next = malloc(sizeof(struct Process));
-				//set new nodes values
-				current = current->next;
-				current->id = location->id;
-				current->arrive = currentSlot->end;
-				current->duration = location->duration - currentSlot->duration;
-				current->priority = location->priority;
-				current->next = 0;
-				//set the current node back to the one we were on.
-				current = location;
-			}
-		}
-	}
-	
-	return rootSlot;
-}
+struct cpuSlot* rr(struct Process* root, int quantum, int* downTime);
 struct cpuSlot* priority_non(struct Process* root, int quantum, int* downTime) {
 	
 	// Sort the list of processes by priority first, arrival time second, and PID third (all lowest-first)
@@ -764,9 +660,9 @@ int main() {
 		exit(1);
 	}
 	
-	//print out the linked list of processes, simply for testing purposes at this point, but
-	//it could be nice just to give the user some more feedback anyways
-	printf("\nThe schedule policy is as follows: %s \n", policy);
+	// Print out the list of processes -- for debugging
+	// (Uncomment from here to the end of the while-loop if you want a little more verbose output.)
+	/*printf("\nThe schedule policy is as follows: %s \n", policy);
 	printf("The quantum for the policy is: %d \n", quantum);
 	printf("The input file has provided these processes:\n");
 	current = rootProcess;
@@ -777,14 +673,16 @@ int main() {
 		printf( "duration: %d ", current->duration);
 		printf( "priority: %d \n", current->priority);
 		current = current->next;
-	}
+	}*/
 	
 	int wait = 0;
 	int turnaround = 0;
 	int downTime = 0;
+
+	struct cpuSlot *slots;
 	
 	if (strcmp(policy, "fcfs") == 0) {
-		struct cpuSlot* slots = fcfs(rootProcess, quantum, &downTime);
+		slots = fcfs(rootProcess, quantum, &downTime);
 		printf("%d ", totalTime(slots));  
 		printf("%d \n", totalTime(slots) + downTime);
 		int count = 0;
@@ -802,7 +700,6 @@ int main() {
 		printf("%f ", (float)turnaround/(float)count);
 		printf("%f \n", (float)wait/(float)count);
 	} else if (strcmp(policy, "rr") == 0) {
-		struct cpuSlot* slots;
 		slots = rr(rootProcess, quantum, &downTime);
 		printf("%d ", totalTime(slots));
 		printf("%d \n", totalTime(slots) + downTime);
@@ -819,9 +716,9 @@ int main() {
 			count++;
 		}
 		printf("%f ", (float)turnaround/(float)count);
-		printf("%f ", (float)wait/(float)count);
+		printf("%f \n", (float)wait/(float)count);
 	} else if (strcmp(policy, "priority_non") == 0) {
-		struct cpuSlot* slots = priority_non(rootProcess, quantum, &downTime);
+		slots = priority_non(rootProcess, quantum, &downTime);
 		printf("%d ", totalTime(slots));  
 		printf("%d \n", totalTime(slots) + downTime);
 		int count = 0;
@@ -839,7 +736,7 @@ int main() {
 		printf("%f ", (float)turnaround/(float)count);
 		printf("%f \n", (float)wait/(float)count);
 	} else if (strcmp(policy, "priority") == 0) {
-		struct cpuSlot* slots = priority(rootProcess, quantum, &downTime);
+		slots = priority(rootProcess, quantum, &downTime);
 		printf("%d ", totalTime(slots));  
 		printf("%d \n", totalTime(slots) + downTime);
 		int count = 0;
@@ -860,9 +757,240 @@ int main() {
 		printf("The schedule policy in the input file is invalid\n");
 	}
 
-	// TODO: traverse the process and CPU-slot lists and free memory
+	// Deallocate the process and CPU-slot lists
+	for(struct Process *p = rootProcess; p != 0; )
+	{
+		struct Process *toDelete = p;
+		p = p->next;
+		free(toDelete);
+	}
+	rootProcess = 0;
+	for(struct cpuSlot *p = slots; p != 0; )
+	{
+		struct cpuSlot *toDelete = p;
+		p = p->next;
+		free(toDelete);
+	}
+	slots = 0;
 	
 	return 0;
+}
+
+struct cpuSlot* rr(struct Process* root, int quantum, int* downTime)
+{
+	// Sort the list of processes by arrival time (with PID as second criteria, but we don't need
+	// to handle that explicitly since the input is guaranteed to be ordered by increasing PID).
+
+	// Create a Process_sort linked list to keep track of the processs during sorting
+	// (we'll take a total of the # of processes while we're at it)
+	int numProcesses = 1;
+	struct Process *current = root;
+	struct Process_sort *sortHead = malloc(sizeof(struct Process_sort));
+	sortHead->proc = root;
+	sortHead->selected = false;
+	sortHead->next = 0;
+	struct Process_sort *currentSort = sortHead;
+	while(current->next != 0)
+	{
+		current = current->next;
+
+		currentSort->next = malloc(sizeof(struct Process_sort));
+		currentSort = currentSort->next;
+
+		currentSort->proc = current;
+		currentSort->selected = false;
+		currentSort->next = 0;
+
+		numProcesses++;
+	}
+
+	// Sort the processes using an insertion sort
+	struct Process sortedListHead; // dummy node at the head of the new sorted list to make things easier
+	current = &sortedListHead;
+	while(true)
+	{
+		// Traverse the list to find the least node that we haven't yet selected (put into place in the sorted list)
+		struct Process_sort *least = 0;
+		
+		currentSort = sortHead;
+		while(currentSort != 0)
+		{
+			if(!currentSort->selected) // we haven't yet put this element into place in the sorted list
+			{
+				if(least == 0 || currentSort->proc->arrive < least->proc->arrive) // we've found a new least
+				{
+					// Unselect the old least (if we have one)
+					if(least)
+						least->selected = false;
+					least = currentSort;
+					// Select the new one
+					least->selected = true;
+				}
+			}
+
+			currentSort = currentSort->next;
+		}
+
+		if(least) // If we found an unselected node
+		{
+			// Add the just-found next-least node to the new, sorted list
+			// (note that in the process, we're destroying the original non-sorted list - that's
+			// OK, since we have pointers to all nodes saved in the Process_sort list and are thus
+			// going to reconstruct the full list in the sorting process)
+			current->next = least->proc;
+			current->next->next = 0;
+			current = current->next;
+		}
+		else // all nodes are selected, so sorting is done
+			break;
+	}
+	// Point root to the head of the new sorted list
+	root = sortedListHead.next;
+
+	// Deallocate the Process_sort list (no longer needed since sorting is complete)
+	for(struct Process_sort *p = sortHead; p->next != 0; )
+	{
+		struct Process_sort *toDelete = p->next;
+		p->next = p->next->next;
+		free(toDelete);
+	}
+	free(sortHead); sortHead = 0;
+
+	// The processes are now sorted; proceed with scheduling.
+
+	// Set up list of slots
+	// (root is a dummy node, to work better with how I've structured my loop; it'll be removed at the end for consistency with the rest of the program)
+	struct cpuSlot *rootSlot = malloc(sizeof(struct cpuSlot));
+	rootSlot->next = 0;
+	struct cpuSlot *pSlot = rootSlot;
+
+	// Keep a "queue" (another list) of just the processes that have arrived, i.e. are subject to scheduling.
+	struct ReadyQueueNode *readyQueue = malloc(sizeof(struct ReadyQueueNode)); // dummy node
+	readyQueue->proc = 0;
+	readyQueue->next = 0;
+	readyQueue->timeRemaining = 0;
+	struct ReadyQueueNode *endReadyQueue = readyQueue; // points to last node in ready queue
+	int processesInQueue = 0;
+
+	// Each loop iteration schedules one process and runs it for one quantum, or until completion if it can finish in less than a quantum
+	int startTime = 0;
+	int prevStartTime = -1;
+	struct ReadyQueueNode *lastScheduled = readyQueue; // start at dummy node
+	while(true)
+	{
+		// Add to the ready queue any processes that have arrived between prevStartTime and startTime
+		for(struct Process *p = root; p != 0; p = p->next)
+		{
+			if(p->arrive > prevStartTime && p->arrive <= startTime)
+			{
+				struct ReadyQueueNode *newReadyProc = malloc(sizeof(struct ReadyQueueNode));
+				newReadyProc->next = 0;
+				newReadyProc->proc = p;
+				newReadyProc->timeRemaining = p->duration;
+
+				endReadyQueue->next = newReadyProc;
+				endReadyQueue = newReadyProc;
+
+				processesInQueue++;
+			}
+		}
+
+		prevStartTime = startTime;
+
+		// If lastScheduled = 0, schedule the first process in the ready queue.
+		// Otherwise, schedule the next process in the ready queue. If we reach the end, wrap around to the beginning.
+		// (If we wrap all the way around to lastScheduled without scheduling a process, schedule it again - it's the only process in the ready queue.)
+		// Finished processes will be left in the queue, but skipped because their timeRemaining is 0.
+		bool scheduledAProcess = false;
+		for(struct ReadyQueueNode *r = lastScheduled->next; r != lastScheduled; r = r->next)
+		{
+			if(r == 0) // end of queue
+				r = readyQueue->next; // return to beginning (skipping dummy node)
+
+			if(r->timeRemaining != 0) // process still has work to be done - schedule it
+			{
+				struct cpuSlot *newSlot = malloc(sizeof(struct cpuSlot));
+				newSlot->next = 0;
+
+				newSlot->procId = r->proc->id;
+				newSlot->startTime = startTime;
+				if(r->timeRemaining > quantum)
+					newSlot->duration = quantum;
+				else
+					newSlot->duration = r->timeRemaining;
+				newSlot->wait = 0; // will calculate this per-process at the end
+				newSlot->end = startTime + newSlot->duration;
+				newSlot->turnaround = 0; // same as wait
+
+				r->timeRemaining -= newSlot->duration;
+
+				startTime += newSlot->duration;
+
+				// Add the new slot to the slots list
+				pSlot->next = newSlot;
+				pSlot = newSlot;
+
+				scheduledAProcess = true;
+				lastScheduled = r;
+				break;
+			}
+		}
+		// If we made it all the way around without scheduling a process, and lastScheduled still has time remaining, there's only one
+		// process in the queue - schedule it. If lastScheduled is finished, the queue is empty, and we should advance one second (idle).
+		if(!scheduledAProcess)
+		{
+			if(lastScheduled->timeRemaining > 0) // one process in queue
+			{
+				struct cpuSlot *newSlot = malloc(sizeof(struct cpuSlot));
+				newSlot->next = 0;
+
+				newSlot->procId = lastScheduled->proc->id;
+				newSlot->startTime = startTime;
+				if(lastScheduled->timeRemaining > quantum)
+					newSlot->duration = quantum;
+				else
+					newSlot->duration = lastScheduled->timeRemaining;
+				newSlot->wait = 0; // will calculate this per-process at the end
+				newSlot->end = startTime + newSlot->duration;
+				newSlot->turnaround = 0; // same as wait
+
+				lastScheduled->timeRemaining -= newSlot->duration;
+
+				// Add the new slot to the slots list
+				pSlot->next = newSlot;
+				pSlot = newSlot;
+			}
+			else // the queue is empty
+			{
+				// Advance one second (idle)
+				startTime++;
+				(*downTime)++;
+
+				if(processesInQueue == numProcesses) // all processes have arrived, and finished - we're done
+					break;
+			}
+		}
+	}
+
+	// Deallocate the ready queue
+	for(struct ReadyQueueNode *p = readyQueue; p != 0; )
+	{
+		struct ReadyQueueNode *toDelete = p;
+		p = p->next;
+		free(toDelete);
+	}
+
+	// Postprocess the slot list to turn it into a per-process summary, sorted by PID
+	struct cpuSlot *summaryList = postprocessSchedulingData(rootSlot->next, root, numProcesses);
+
+	// Deallocate the unprocessed slot list, and return the processed one
+	for(struct cpuSlot *p = rootSlot; p != 0; )
+	{
+		struct cpuSlot *toDelete = p;
+		p = p->next;
+		free(toDelete);
+	}
+	return summaryList;
 }
 
 struct cpuSlot* postprocessSchedulingData(struct cpuSlot *slotList, struct Process *procList, int numProcesses)
