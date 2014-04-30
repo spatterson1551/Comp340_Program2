017a2caf2839524b4cb0e3aa2a825aac38204cfd
Ethan J. Johnson <ethanjohnson89@gmail.com>
2014-04-30 12:09:21 -0400

    Various fixes to task #3 - seems to work now

--- Program3/task3.c
+++ Program3/task3.c
@@ -6,36 +6,19 @@
 #include <string.h>
 #include <stdbool.h>
 
-// defines to help keep track of pipe ends
-#define READ_END 0
-#define WRITE_END 1
-
-#define MEM_THRESHOLD 75.0f // percentage of system memory usage we want to watch for
+#define MEM_THRESHOLD 0.80f // fraction of system memory usage we want to watch for
 #define CHECK_FREQUENCY 1 // number of seconds to wait between checking system memory usage
 
 // Check periodically to see if system memory usage exceeds a defined threshold; when it goes over that threshold, print a message to the console reflecting this.
 int main()
 {
+	printf("Monitoring memory usage; press Ctrl-C to quit.\n");
+
 	bool previouslyOverThreshold = false;
 
-	// Main loop - continue running until the user presses the "Q" key
+	// Main loop - continue running until the user presses Ctrl-C
 	while(true)
 	{
-		// Read any user input that's accumulated since the last iteration and see if the "Q" key was pressed to terminate the program
-		char inputBuffer[20];
-		size_t bytesRead = -1;
-		bool qPressed = false;
-		do
-		{
-			bytesRead = fread(inputBuffer, sizeof(char), 20, stdin);
-			for(int i = 0; i < bytesRead; i++)
-				if(inputBuffer[i] == 'q' || inputBuffer[i] == 'Q')
-					qPressed = true;
-		} while(bytesRead == 20); // continue until all accumulated input has been consumed
-
-		if(qPressed) // if Q was pressed, stop looping and end the program
-			break;
-
 		//// Read from /proc/meminfo to determine system memory usage
 		FILE *meminfo = fopen("/proc/meminfo", "r"); // open /proc/meminfo for reading
 		size_t lineLength = 0;
@@ -55,13 +38,14 @@ int main()
 		// Parse the total/free memory values from the lines
 		int memTotal, memFree;
 		result1 = result2 = -1;
-		result1 = sscanf(memTotalLine, "MemTotal: %d kB", memTotal);
-		result2 = sscanf(memFreeLine, "MemFree: %d kB", memFree);
+		result1 = sscanf(memTotalLine, "MemTotal: %d kB", &memTotal);
+		result2 = sscanf(memFreeLine, "MemFree: %d kB", &memFree);
 		if(result1 != 1 || result2 != 1)
 		{
 			fprintf(stderr, "Error: failed to parse MemTotal and MemFree from /proc/meminfo contents!\n");
 			free(memTotalLine);
 			free(memFreeLine);
+			fclose(meminfo);
 
 			// Sleep for CHECK_FREQUENCY seconds and proceed to the next check (the error might be a transient condition, so we don't want to quit)
 			sleep(CHECK_FREQUENCY);
@@ -69,6 +53,7 @@ int main()
 		}
 		free(memTotalLine);
 		free(memFreeLine);
+		fclose(meminfo);
 
 		// If system memory usage is over MEM_THRESHOLD%, and this wasn't the case on the last iteration, print a message to the console
 		float memFreePct = (float)memFree / (float)memTotal;
@@ -76,7 +61,7 @@ int main()
 		{
 			if(!previouslyOverThreshold)
 			{
-				printf("Memory usage exceeded threshold (%f%%): %f\n", MEM_THRESHOLD, 1.0f - memFreePct);
+				printf("Memory usage exceeded threshold (%f%%): %f\n", 100*MEM_THRESHOLD, 100*(1.0f - memFreePct));
 				previouslyOverThreshold = true;
 			}
 		}
